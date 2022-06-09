b4db3eedd3b15d30c679b88bf8b959d24965b56c
mardisen <marino.didio.work@gmail.com>
2022-06-09 19:00:31 +0200

    Added TODOs

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import type { Component } from 'solid-js';
 import BaseScene from './components/BaseScene';
 import SwipeCard from './components/SwipeCard';
 
-//TODO: Use card based layout for information and animate it like tinder swipes
+// TODO: Use card based layout for information and animate it like tinder swipes
 const App: Component = () => {
   return (
     <div class="flex flex-col">
       <div class="text-4xl text-green-700 text-center py-20 z-20 relative w-full">Hello tailwind!</div>
       <BaseScene class="z-10 absolute" />
-      <SwipeCard class="z-20 relative h-24 w-24 justify-center m-auto flex items-center bg-slate-400 rounded">Card 1</SwipeCard>
+      <SwipeCard class="z-30 relative h-24 w-24 justify-center m-auto flex items-center bg-slate-400 rounded">Card 1</SwipeCard>
     </div>
   );
 };
--- src/components/SwipeCard.tsx
+++ src/components/SwipeCard.tsx
@@ -1,7 +1,20 @@
 import type { JSX, ParentComponent, ParentProps } from 'solid-js';
 
+// TODO: Calculate Speed
+// TODO: Calculate Angle
+// TODO: Animate basic Drag
+    // TODO: Define transition
+    // TODO: Define animation
+// TODO: Animate on release
+// TODO: Animate bring back
+
 type Props = {
     class?: string;
+};
+
+type Coordinate = {
+    x: number;
+    y: number;
 };
 
 const onMouseMove: JSX.EventHandlerUnion<HTMLDivElement, MouseEvent> =
@@ -9,10 +22,18 @@ const onMouseMove: JSX.EventHandlerUnion<HTMLDivElement, MouseEvent> =
         console.info(event.clientX, event.clientY);
     };
 
+const onTouchMove: JSX.EventHandlerUnion<HTMLDivElement, TouchEvent> =
+    (event) => {
+        console.info(event.touches[0].clientX, event.touches[0].clientY);
+    };
+
 const SwipeCard: ParentComponent<Props> = (props: ParentProps<Props>) => {
+
+
     return <div
         class={props.class}
         onMouseMove={onMouseMove}
+        onTouchMove={onTouchMove}
     >
         {props.children}
     </div>;
