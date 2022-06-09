25acbfb6002933a14b6e9c262027ad04a70a9b1e
mardisen <marino.didio.work@gmail.com>
2022-06-09 17:48:52 +0200

    Improved BaseScene

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import type { Component } from 'solid-js';
 import BaseScene from './components/BaseScene';
 
+//TODO: Use card based layout for information and animate it like tinder swipes
 const App: Component = () => {
-  return (<>
-    <p class="text-4xl text-green-700 text-center py-20">Hello tailwind!</p>
-    <BaseScene />
-  </>
-
+  return (
+    <div class="relative">
+      <div class="text-4xl text-green-700 text-center py-20 z-20 absolute w-full">Hello tailwind!</div>
+      <div class="relative z-10">
+        <BaseScene class="z-10 relative" />
+      </div>
+    </div>
   );
 };
 
--- src/components/BaseScene.tsx
+++ src/components/BaseScene.tsx
@@ -1,9 +1,12 @@
 import type { Component } from 'solid-js';
 import { WebGLRenderer, PerspectiveCamera, Scene, LineBasicMaterial, Vector3, BufferGeometry, Line } from 'three';
 
-const BaseScene: Component = () => {
+type Props = {
+    class: string;
+};
+
+const BaseScene: Component<Props> = (props: Props) => {
     const renderer = new WebGLRenderer();
-    document.body.appendChild(renderer.domElement);
 
     const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
     camera.position.set(0, 0, 100);
@@ -11,19 +14,10 @@ const BaseScene: Component = () => {
 
     const scene = new Scene();
 
-    const material = new LineBasicMaterial({ color: 0x0000ff });
-
-    const points = [];
-    points.push(new Vector3(- 10, 0, 0));
-    points.push(new Vector3(0, 10, 0));
-    points.push(new Vector3(10, 0, 0));
-
-    const geometry = new BufferGeometry().setFromPoints(points);
-
-    const line = new Line(geometry, material);
-
-    scene.add(line);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setClearColor( 0xffffff, 0);
     renderer.render(scene, camera);
+    renderer.domElement.className = props.class;
     return renderer.domElement;
 };
 
