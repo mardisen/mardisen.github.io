0ca1e0205e50c85e28ac03137d22d9c640afccbf
mardisen <marino.didio.work@gmail.com>
2022-06-09 18:12:13 +0200

    Prop "class" is now optional

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import type { Component } from 'solid-js';
 import BaseScene from './components/BaseScene';
+import SwipeCard from './components/SwipeCard';
 
 //TODO: Use card based layout for information and animate it like tinder swipes
 const App: Component = () => {
   return (
     <div class="relative">
       <div class="text-4xl text-green-700 text-center py-20 z-20 absolute w-full">Hello tailwind!</div>
-      <div class="relative z-10">
-        <BaseScene class="z-10 relative" />
-      </div>
+      <BaseScene class="z-10 relative" />
     </div>
   );
 };
