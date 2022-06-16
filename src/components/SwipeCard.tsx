c5d5191429c5ad4ec0b31a2682fea88686c61444
mardisen <marino.didio.work@gmail.com>
2022-06-16 07:04:21 +0200

    Implemented onMouseDown and onTouchStart

--- src/components/SwipeCard.tsx
+++ src/components/SwipeCard.tsx
@@ -1,10 +1,10 @@
-import type { JSX, ParentComponent, ParentProps } from 'solid-js';
+import { createMemo, createSignal, JSX, ParentComponent, ParentProps } from 'solid-js';
 
 // TODO: Calculate Speed DONE
 // TODO: Calculate Angle DONE
 // TODO: Animate basic Drag
-    // TODO: Define transition
-    // TODO: Define animation
+// TODO: Define transition
+// TODO: Define animation
 // TODO: Animate on release
 // TODO: Animate bring back
 
@@ -38,6 +38,9 @@ const pythagoras = (coords: Coordinate): number => {
     return Math.sqrt(Math.pow(coords.x, 2) + Math.pow(coords.y, 2));
 };
 
+const mouseCoordinates = (event: MouseEvent): Coordinate => ({ x: event.clientX, y: event.clientY });
+const touchCoordinates = (event: TouchEvent): Coordinate => ({ x: event.touches[0].clientX, y: event.touches[0].clientY });
+
 const onMouseMove: JSX.EventHandlerUnion<HTMLDivElement, MouseEvent> =
     (event) => {
         console.info(event.clientX, event.clientY);
@@ -48,13 +51,33 @@ const onTouchMove: JSX.EventHandlerUnion<HTMLDivElement, TouchEvent> =
         console.info(event.touches[0].clientX, event.touches[0].clientY);
     };
 
+
 const SwipeCard: ParentComponent<Props> = (props: ParentProps<Props>) => {
+    const [isDragging, setIsDragging] = createSignal(false);
+    const [offset, setOffset] = createSignal<Coordinate>({ x: 0, y: 0 });
+
+    const onMouseDown: JSX.EventHandlerUnion<HTMLDivElement, MouseEvent> =
+        (event) => {
+            event.preventDefault();
+            setIsDragging(false);
+            setOffset(mouseCoordinates(event));
+        };
+
+    const onTouchStart: JSX.EventHandlerUnion<HTMLDivElement, TouchEvent> =
+        (event) => {
+            event.preventDefault();
+            setIsDragging(false);
+            setOffset(touchCoordinates(event));
+        };
+
 
 
     return <div
         class={props.class}
         onMouseMove={onMouseMove}
         onTouchMove={onTouchMove}
+        onMouseDown={onMouseDown}
+        onTouchStart={onTouchStart}
     >
         {props.children}
     </div>;
