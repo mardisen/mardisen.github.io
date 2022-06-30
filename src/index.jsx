3c69ba9dbaa78ae89951196a70bb3dde1c4b7578
mardisen <marino.didio.work@gmail.com>
2022-06-30 07:48:38 +0200

    Fix for GitHub Pages deployement

--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,4 @@ import { render } from 'solid-js/web';
 
 import App from './App';
 
-render(() => <App />, document.getElementById('root') as HTMLElement);
+render(() => <App />, document.getElementById('root'));
